0765e5e288dbd02337a27d894914f4f6f8be26e0
asterix-e42 <tybo89@live.fr>
2019-12-05 23:32:45 +0100

    Update ft_strlen.s

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -1,29 +1,13 @@
-
 section .text
 global	_ft_strlen
 
 _ft_strlen :
-	push rbp
-	mov rbp, rsp
-
 	xor rax, rax
-
-	cmp rdi, 0
-	je _end
-
 	xor rcx, rcx
 	not rcx
-	
 	cld
-
 	repne scasb
-
 	not rcx
-	sub rdi, rcx
 	dec rcx
 	mov rax, rcx
-
-
-_end :
-	leave
 	ret
